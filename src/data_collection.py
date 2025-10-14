1b80cf6b5c5768766f11205cc3c887a1eb60a1c2
Razan Almasood <r.almasooud@gmail.com>
2025-10-14 16:27:14 -0500

    implement HTML request and parsing for US News rankings

--- src/data_collection.py
+++ src/data_collection.py
@@ -10,7 +10,16 @@ import pandas as pd
 
 def scrape_usnews_top50():
     """Base function setup for Top 50 US News scraper"""
-    pass
+        url = 
+"https://www.usnews.com/best-colleges/rankings/national-universities"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        raise Exception("Failed to fetch data from U.S. News")
+
+    soup = BeautifulSoup(response.text, "lxml")
+    print("Page fetched and parsed successfully")
 
 
 if __name__ == "__main__":
