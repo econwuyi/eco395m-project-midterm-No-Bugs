0e24321189ed0c45e2c6b4335b353b9187643140
Razan Almasood <r.almasooud@gmail.com>
2025-10-14 16:21:17 -0500

    add base scrape_usnews_top50() function setup

---
+++ src/data_collection.py
@@ -0,0 +1,18 @@
+"""
+Step 2 — Scrape tuition fees and SAT score ranges
+for the Top 50 U.S. News National Universities (2026).
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+def scrape_usnews_top50():
+    """Base function setup for Top 50 US News scraper"""
+    pass
+
+
+if __name__ == "__main__":
+    scrape_usnews_top50()
+
